eb3e1c01f54d5c9af056798da501eee0eead25ec
alexquiva2005-afk <alexquiva2005@gmail.com>
2026-06-07 21:56:23 +0200

    Delete src directory

--- src/utils/roomGenerator.js
+++
@@ -1,9 +0,0 @@
-// src/utils/roomGenerator.js
-export const generarCodigoSala = () => {
-  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let resultado = '';
-  for (let i = 0; i < 6; i++) {
-    resultado += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
-  }
-  return resultado;
-};
